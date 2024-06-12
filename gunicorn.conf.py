ec0ca1b535b491b4ae22a51ef4464d7beb7d1c12
Muhammad Zaheer Naivasal <nmzaheer@srushty.com>
2024-06-12 08:53:16 +0530

    Set up Gunicorn config and logs

---
+++ gunicorn.conf.py
@@ -0,0 +1,12 @@
+# gunicorn.conf.py
+# Non logging stuff
+bind = "0.0.0.0:5000"
+workers = 3
+# Access log - records incoming HTTP requests
+accesslog = "/home/frappe/relay-server/log/gunicorn.access.log"
+# Error log - records Gunicorn server goings-on
+errorlog = "/home/frappe/relay-server/log/gunicorn.error.log"
+# Whether to send Django output to the error log 
+capture_output = True
+# How verbose the Gunicorn error logs should be 
+loglevel = "debug"

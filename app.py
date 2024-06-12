19bdee442c83295875a2c8b2baf6ce4629b5d4e6
Muhammad Zaheer Naivasal <mnzaheer@srushty.com>
2024-06-12 07:08:25 +0530

    Changed response structure, saved mapping to file & used WebPushMessage

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import firebase_admin
+import json
 from firebase_admin import messaging
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_httpauth import HTTPBasicAuth
 from my_secrets import API_SECRET, FIREBASE_CONFIG, VAPID_PUBLIC_KEY
 
@@ -11,7 +12,12 @@ basic_auth = HTTPBasicAuth()
 
 API_KEY = 'pfZOoRCW-kHtAHQcqiDvb_sy5o8Hj14J_ahUBQgPfOI'
 
-USER_DEVICE_MAP = {'raven_erp.srushty.com':{}, 'hrms_erp.srushty.com':{}}
+USER_DEVICE_MAP = {'raven_erpsgs.in':{}, 'hrms_erpsgs.in':{}}
+
+TOPICS = []
+
+with open('user-device-map.json', 'r') as jsonfile:
+		USER_DEVICE_MAP = json.load(jsonfile)
 
 @app.get('/api/method/notification_relay.api.get_config')
 def get_config():
@@ -39,9 +45,11 @@ def subscribe_to_topic():
 		response = messaging.subscribe_to_topic(registration_tokens, topic_name)
 		print(response.success_count, 'tokens were subscribed successfully')
 		response = {
-			'success':'OK',
+		'message':{
+			'success':200,
 			'message':'User subscribed'
-		}
+			}
+	}
 		return response
 	return "User token not registered", 400
 
@@ -58,9 +66,11 @@ def unsubscribe_to_topic():
 		response = messaging.unsubscribe_from_topic(registration_tokens, topic_name)
 		print(response.success_count, 'tokens were unsubscribed successfully')
 		response = {
-			'success':'OK',
-			'message':'User unsubscribed'
-		}
+		'message':{
+			'success':200,
+			'message':'User unsubscriber'
+			}
+	}
 		return response
 	return "User token not registered", 400
 
@@ -76,11 +86,16 @@ def add_token():
 		USER_DEVICE_MAP[key][user_id].append(fcm_token)
 	else:
 		USER_DEVICE_MAP[key][user_id] = [fcm_token]
+	with open('user-device-map.json', 'w') as jsonfile:
+		json.dump(USER_DEVICE_MAP, jsonfile)
 	response = {
-		'success':'OK',
-		'message':'User Token added'
+		'message':{
+			'success':200,
+			'message':'User Token added'
+			}
 	}
-	return response
+	print(response)
+	return jsonify(response)
 
 @app.post("/api/method/notification_relay.api.token.remove")
 @basic_auth.login_required
@@ -92,9 +107,13 @@ def remove_token():
 	fcm_token = request.args.get('fcm_token')
 	if user_id in USER_DEVICE_MAP.get(key):
 		USER_DEVICE_MAP[key][user_id].remove(fcm_token)
+	with open('user-device-map.json', 'w') as jsonfile:
+		json.dump(USER_DEVICE_MAP, jsonfile)
 	response = {
-		'success':'OK',
-		'message':'User Token removed'
+		'message':{
+			'success':200,
+			'message':'User Token removed'
+			}
 	}
 	return response
 
@@ -107,21 +126,29 @@ def send_notification_to_user():
 	user_id = request.args.get('user_id')
 	title = request.args.get('title')
 	body = request.args.get('body')
-	data = request.args.get('data')
+	data = json.loads(request.args.get('data'))
 	registration_tokens = []
 	if user_id in USER_DEVICE_MAP.get(key):
 		registration_tokens = USER_DEVICE_MAP.get(key).get(user_id)
 		message = messaging.MulticastMessage(
-			title=title,
-			body=body,
-			data=data,
+			webpush=messaging.WebpushConfig(
+				notification=messaging.WebpushNotification(
+					title=title,
+					body=body,
+					icon='https://erpsgs.in/assets/raven/raven_mobile/favicon-32x32.png',
+				),
+				fcm_options=messaging.WebpushFCMOptions(link=data.get('click_action')),
+			),
 			tokens=registration_tokens
 		)
-		response = messaging.send(message)
+		response = messaging.send_multicast(message)
 		print('Successfully sent message:', response)
+		
 		response = {
-			'success':'OK',
-			'message':f'Notiifcation sent to {user_id} user'
+			'message':{
+				'success':200,
+				'message':f'Notiifcation sent to {user_id} user'
+				}
 		}
 		return response
 	return 'User registration not found', 400
@@ -132,17 +159,24 @@ def send_notification_to_topic():
 	topic = request.args.get('topic_name')
 	title = request.args.get('title')
 	body = request.args.get('body')
-	data = request.args.get('data')
+	data = json.loads(request.args.get('data'))
 	message = messaging.Message(
-		title=title,
-		body=body,
-    	data=data,
-    	topic=topic,
-	)
+        webpush=messaging.WebpushConfig(
+            notification=messaging.WebpushNotification(
+                title=title,
+                body=body,
+                icon='https://erpsgs.in/assets/raven/raven_mobile/favicon-32x32.png',
+            ),
+			fcm_options=messaging.WebpushFCMOptions(link=data.get('click_action')),
+        ),
+        topic=topic,
+    )
 	response = messaging.send(message)
 	print('Successfully sent message:', response)
 	response = {
-		'success':'OK',
-		'message':f'Notiifcation sent to {topic} topic'
+		'message':{
+			'success':200,
+			'message':f'Notiifcation sent to {topic} topic'
+			}
 	}
 	return response

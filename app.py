d845f44dc1b74e6f59f4c467dfb12a1595327800
tuan.truong <tuan.truong@widosoft.com>
2025-07-17 09:58:37 +0700

    Remove local development server run command from app.py

--- app.py
+++ app.py
@@ -258,5 +258,3 @@ if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8899, debug=True)

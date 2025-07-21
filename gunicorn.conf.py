d33201fd5517da8653b0b7239251eeceee246e88
tuan.truong <tuan.truong@widosoft.com>
2025-07-21 11:17:03 +0700

    Add Dockerfile and update Gunicorn configuration for deployment

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,11 @@
 # gunicorn.conf.py
 # Non logging stuff
-bind = "0.0.0.0:5000"
+bind = "0.0.0.0:8080"
 workers = 1
 # Access log - records incoming HTTP requests
-accesslog = "/home/dev/workspace/projects/frappe/relay-server/relay-server/log/gunicorn.access.log"
+accesslog = "./log/gunicorn.access.log"
 # Error log - records Gunicorn server goings-on
-errorlog = "/home/dev/workspace/projects/frappe/relay-server/relay-server/log/gunicorn.error.log"
+errorlog = "./log/gunicorn.error.log"
 # Whether to send Django output to the error log
 capture_output = True
 # How verbose the Gunicorn error logs should be 
